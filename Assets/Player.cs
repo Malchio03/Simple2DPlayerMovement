b6157c5163844bdc6c90b1caa93ebc13ec6a7ccf
Malchio03 <malchio1753@gmail.com>
2025-07-12 19:42:54 +0200

    Jump Logic

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -5,6 +5,7 @@ public class Player : MonoBehaviour
     private Rigidbody2D rb;
 
     [SerializeField] private float moveSpeed = 3.5f;
+    [SerializeField] private float jumpForce = 8f;
     private float xInput;
 
     private void Awake()
@@ -14,9 +15,28 @@ public class Player : MonoBehaviour
 
     private void Update()
     {
+        HandleInput();
+        HandleMovement();
+    }
+
+    private void HandleInput()
+    {
         xInput = Input.GetAxis("Horizontal");
 
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            Jump();
+        }
+    }
+
+    private void HandleMovement()
+    {
         // update the player's horizontal velocity based on input
         rb.linearVelocity = new Vector2(xInput * moveSpeed, rb.linearVelocity.y);
+    }
+
+    private void Jump()
+    {
+        rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
     }
 }
